9bd264d549f898eae761e4a80ac6d6bec7384339
orange-orunji <3078045959@qq.com>
2026-06-15 10:45:37 +0800

    登录相关先前接口完善

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -5,5 +5,5 @@ class User(Base):
     __tablename__ = "users" # 定义表名
 
     id = Column(Integer,primary_key=True,index=True)
-    user_name = Column(String,unique=True,index=True)
+    username = Column(String,unique=True,index=True)
     hashed_password = Column(String) #存储加密后的暗文密码所以用字符串来接受,对比的是哈希值
--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,31 +1,28 @@
-from dns.rdtypes import ANY
-from jose import JWTError,jwt
+from jose import jwt
 from passlib.context import CryptContext
 from datetime import datetime,timedelta
 
 SECRET_ID = "my_secret_key_for_demo" # 加密前缀
 ALGORITHM = "HS256" # 加密方式
-expire_time = 60*24*7 # JWT令牌存在时间
+ACCESS_TOKEN_EXPIRE_MINUTES = 60*24*7 # JWT令牌存在时间
 # 生成加密上下文文本对象
-pwd_content = CryptContext(schemes=["bcrypt"],deprecated="auto")
+pwd_content = CryptContext(schemes=["pbkdf2_sha256"],deprecated="auto")
 
 def generate_hash_password(password : str) -> str:
     """加密明文密码成指定的加密后的密码"""
     return pwd_content.hash(password)
 
-def verify_password(target_value : str ,hash_password : str) -> bool | ANY:
+def verify_password(plain_password: str, hashed_password: str) -> bool:
     """校验密码和目标哈希值是否一样"""
-    return pwd_content.verify(hash_password,target_value)
+    return pwd_content.verify(plain_password, hashed_password)
 
-def create_access_token(data : dict) -> str | type[JWTError]:
+
+def create_access_token(data: dict) -> str:
     """生成JWT令牌方法"""
-    base_token = data.copy()
-    try:
-        expire = datetime.now() + timedelta(expire_time)
-        base_token.update({"exp":expire})
-        return jwt.encode(base_token,SECRET_ID,algorithm=ALGORITHM)
-    except JWTError:
-        return JWTError
+    to_encode = data.copy()
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_ID, algorithm=ALGORITHM)
 
 
 def decode_token(token : str) -> dict:
